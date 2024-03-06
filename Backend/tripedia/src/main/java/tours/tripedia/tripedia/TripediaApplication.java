db8549b858287242e8a6d9e97b00ed43e4be28c1
Shyccf <h235shi@uwaterloo.ca>
2024-03-06 16:30:13 -0500

    Initial framework

---
+++ Backend/tripedia/src/main/java/tours/tripedia/tripedia/TripediaApplication.java
@@ -0,0 +1,13 @@
+package tours.tripedia.tripedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripediaApplication.class, args);
+	}
+
+}
---
+++ Backend/tripedia/src/test/java/tours/tripedia/tripedia/TripediaApplicationTests.java
@@ -0,0 +1,13 @@
+package tours.tripedia.tripedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
