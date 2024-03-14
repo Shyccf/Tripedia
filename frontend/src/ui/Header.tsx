681618e424d899a00dd874ad1d04231fbead8a97
z297xu <kyrie807782352@gmail.com>
2024-03-13 21:13:47 -0400

    change fonts + Header init

--- frontend/src/ui/Header.tsx
+++ frontend/src/ui/Header.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Logo from "/images/logo.png";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="header container flex justify-between items-center mx-auto bg-slate-300 px-8">
+      <img src={Logo} className="w-32 mt-4" alt="logo" />
 
-export default Header
+      <div>SearchBar</div>
+
+      <ul className="flex gap-4">
+        <li>Home</li>
+        <li>Trips</li>
+        <li>Blogs</li>
+      </ul>
+
+      <div>userImage</div>
+    </div>
+  );
+};
+
+export default Header;
