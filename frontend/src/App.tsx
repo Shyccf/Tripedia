16338f40a2f59786b2cc91f4855bc34922a44684
z297xu <kyrie807782352@gmail.com>
2024-03-13 20:29:45 -0400

    Build up the structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,52 @@
-import './App.css'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import AppLayout from "./ui/AppLayout";
+import Blog from "./features/blog/Blog";
+import CreateBlog from "./features/blog/CreateBlog";
+import BlogOverview from "./features/blog/BlogOverview";
+import City from "./features/trip/City";
+import Trip from "./features/trip/Trip";
+import User from "./features/user/User";
+import Home from "./features/homepage/Home";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/user",
+        element: <User />,
+      },
+      {
+        path: "/trip",
+        element: <Trip />,
+      },
+      {
+        path: "/trip/:cityId",
+        element: <City />,
+      },
+      {
+        path: "/blog",
+        element: <BlogOverview />,
+      },
+      {
+        path: "/blog/new",
+        element: <CreateBlog />,
+      },
+      {
+        path: "/blog/:blogId",
+        element: <Blog />,
+      },
+    ],
+  },
+]);
 
 function App() {
-
-  return (
-    <>
-      <h1 className='bg-red-200'>Hello World</h1>
-      <h2 className='bg-blue-100'>Test</h2>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
