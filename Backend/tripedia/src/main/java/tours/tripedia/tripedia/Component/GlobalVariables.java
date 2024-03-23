601f3d1c99bb6a617a000a4dd3e944a1e479fb4b
Shyccf <h235shi@uwaterloo.ca>
2024-03-23 14:11:53 -0400

    Update GlobalVariables.java

--- Backend/tripedia/src/main/java/tours/tripedia/tripedia/Component/GlobalVariables.java
+++ Backend/tripedia/src/main/java/tours/tripedia/tripedia/Component/GlobalVariables.java
@@ -4,5 +4,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class GlobalVariables {
-    public static final String crossOriginUrl = "http://frontend";
+    // public static final String crossOriginUrl = "http://frontend";
+    public static final String crossOriginUrl = "http://localhost:3000";
 }
