879ff10a08099b740925ba4961d88897f965d1e2
z297xu <kyrie807782352@gmail.com>
2024-04-03 18:55:31 -0400

    make carousel component more general

--- frontend/src/components/Carousel.tsx
+++ frontend/src/components/Carousel.tsx
@@ -3,7 +3,7 @@ import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import React, { useState } from "react";
 
-const Carousel: React.FC<CarouselProps> = ({ slides }) => {
+const Carousel: React.FC<CarouselProps> = ({ slides, buttons=true, thumbnail=true }) => {
   const [curPage, setCurPage] = useState(0);
 
   const handleNext = () => {
@@ -50,92 +50,99 @@ const Carousel: React.FC<CarouselProps> = ({ slides }) => {
         ))}
       </Box>
 
-      {/* Left Button */}
-      <IconButton
-        color="primary"
-        sx={{
-          position: "absolute",
-          top: "50%",
-          left: 16,
-          transform: "translateY(-50%)",
-          padding: 0.5,
-          borderRadius: "50%",
-          backgroundColor: "primary.light",
-          zIndex: 2,
-        }}
-        onClick={handlePrev}
-      >
-        <KeyboardArrowLeftIcon />
-      </IconButton>
-      {/* Right Button */}
-      <IconButton
-        color="primary"
-        sx={{
-          position: "absolute",
-          top: "50%",
-          right: 16,
-          transform: "translateY(-50%)",
-          padding: 0.5,
-          borderRadius: "50%",
-          backgroundColor: "primary.light",
-          zIndex: 2,
-        }}
-        onClick={handleNext}
-      >
-        <KeyboardArrowRightIcon />
-      </IconButton>
+      {buttons && (
+        <>
+          {/* Left Button */}
+          <IconButton
+            color="primary"
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: 32,
+              transform: "translateY(-50%)",
+              padding: 0.5,
+              borderRadius: "50%",
+              backgroundColor: "primary.light",
+              zIndex: 2,
+            }}
+            onClick={handlePrev}
+          >
+            <KeyboardArrowLeftIcon />
+          </IconButton>
+          {/* Right Button */}
+          <IconButton
+            color="primary"
+            sx={{
+              position: "absolute",
+              top: "50%",
+              right: 32,
+              transform: "translateY(-50%)",
+              padding: 0.5,
+              borderRadius: "50%",
+              backgroundColor: "primary.light",
+              zIndex: 2,
+            }}
+            onClick={handleNext}
+          >
+            <KeyboardArrowRightIcon />
+          </IconButton>
+        </>
+      )}
 
-      {/* small-image navigators */}
-      <Box
-        sx={{
-          position: "absolute",
-          // left: 0,
-          top: "0",
-          right: "10%", // 使用百分比或视口单位来实现响应式布局
-          bottom: "0", // 调整导航器在轮播图内的位置
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+      {/* thumbnail navigators */}
+
+      {thumbnail && (
         <Box
           sx={{
+            position: "absolute",
+            // left: 0,
+            top: "0",
+            right: "10%", // 使用百分比或视口单位来实现响应式布局
+            bottom: "0", // 调整导航器在轮播图内的位置
             display: "flex",
-            gap: {
-              xs: 0.5,
-              md: 1,
-              lg: 1.5,
-            },
-            flexDirection: "column",
             justifyContent: "center",
             alignItems: "center",
-            maxWidth: {
-              xs: "3rem",
-              md: "5rem",
-              lg: "6rem",
-            },
-            maxHeight: "2rem",
           }}
         >
-          {slides.map((slide, index) => (
-            <Box
-              key={index}
-              component="img"
-              src={slide.imgPath}
-              sx={{
-                width: "auto",
-                height: "100%",
-                objectFit: "cover",
-                objectPosition: "center",
-                filter: curPage === index ? "" : "grayscale(70%)",
-                transition: "all 0.5s ease-in-out",
-                border: curPage === index ? "5px solid red" : "",
-              }}
-              onClick={() => setCurPage(index)}
-            />
-          ))}
+          <Box
+            sx={{
+              display: "flex",
+              gap: {
+                xs: 0.5,
+                md: 1,
+                lg: 1.5,
+              },
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              maxWidth: {
+                xs: "3rem",
+                md: "5rem",
+                lg: "6rem",
+              },
+              maxHeight: "2rem",
+            }}
+          >
+            {slides.map((slide, index) => (
+              <Box
+                key={index}
+                component="img"
+                src={slide.imgPath}
+                sx={{
+                  width: "auto",
+                  height: "100%",
+                  objectFit: "cover",
+                  objectPosition: "center",
+                  filter: curPage === index ? "" : "grayscale(70%)",
+                  transition: "all 0.5s ease-in-out",
+                  border: curPage === index ? "5px solid red" : "",
+                }}
+                onClick={() => setCurPage(index)}
+              />
+            ))}
+          </Box>
         </Box>
-      </Box>
+      )}
     </Box>
   );
 };
