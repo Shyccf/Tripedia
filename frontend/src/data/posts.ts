f822aec5140b4def57859579baa1e0c72f0d7e3c
ziyi xu <kyrie807782352@gmail.com>
2024-04-13 16:36:29 -0400

    add some data

--- frontend/src/data/posts.ts
+++ frontend/src/data/posts.ts
@@ -33,4 +33,37 @@ export const posts = [
     views: "999",
     likes: "99",
   },
+  {
+    title: "Canada's Wonderland",
+    imgPath: "https://viewthevibe.com/wp-content/uploads/2021/08/Screen-Shot-2021-08-13-at-9.44.49-AM.png",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non tempor libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas...",
+    location: "Vanguard, Canada",
+    author: "Kyrie",
+    date: "04/13/2024",
+    views: "256",
+    likes: "63",
+  },
+  {
+    title: "University of Waterloo",
+    imgPath: "https://scholarly.co/wp-content/uploads/2023/01/15.jpg",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non tempor libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas...",
+    location: "Waterloo, Canada",
+    author: "Chris",
+    date: "02/21/2024",
+    views: "1050",
+    likes: "602",
+  },
+  {
+    title: "McMaster University",
+    imgPath: "https://d2l4kn3pfhqw69.cloudfront.net/wp-content/uploads/2022/08/mcmaster2jpg.jpg",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non tempor libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas...",
+    location: "Hamilton, Canada",
+    author: "Kyrie",
+    date: "03/29/2024",
+    views: "420",
+    likes: "59",
+  },
 ];
