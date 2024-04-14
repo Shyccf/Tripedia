90682fce233fc252b622d31802b1f5c87c6c8279
Shyccf <h235shi@uwaterloo.ca>
2024-04-14 15:39:06 -0400

    update frontend port

--- Backend/tripedia/src/main/java/tours/tripedia/tripedia/Component/GlobalVariables.java
+++ Backend/tripedia/src/main/java/tours/tripedia/tripedia/Component/GlobalVariables.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Component;
 @Component
 public class GlobalVariables {
     // public static final String crossOriginUrl = "http://frontend";
-    public static final String crossOriginUrl = "http://localhost:3000";
+    public static final String crossOriginUrl = "http://localhost:5173";
 }
